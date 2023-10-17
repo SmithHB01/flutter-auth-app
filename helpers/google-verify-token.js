930fb459c2a7a0711f2b4590697f2f7a280865c8
Smith Herrera <brayanshg@gmail.com>
2023-10-17 12:17:46 -0500

    Google ID Token listo

---
+++ helpers/google-verify-token.js
@@ -0,0 +1,42 @@
+
+const {OAuth2Client} = require('google-auth-library');
+
+const CLIENT_ID = '836290926727-oh1ftiijn1e509ebqjto2v4ovmhua4do.apps.googleusercontent.com';
+
+const client = new OAuth2Client(CLIENT_ID);
+
+const validarGoogleIdToken = async ( token ) => {
+
+    try {
+
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: [
+                CLIENT_ID,
+                '836290926727-aa3n55ofrmftbf4a1uaop9ukds5jfea5.apps.googleusercontent.com'
+            ],  
+        
+        });
+        const payload = ticket.getPayload();
+
+        return {
+            name: payload['name'],
+            picture: payload['picture'],
+            email: payload['email'],
+            
+
+        }
+        
+    } catch (error) {
+        return null;
+        
+    }
+
+    
+
+
+}
+
+module.exports = {
+    validarGoogleIdToken
+}
